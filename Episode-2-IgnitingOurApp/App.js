69776a8976b5c57e9aacdb296954f430d59c2f61
Pritam Chauhan <rajputpritam619@gmail.com>
2023-08-22 22:52:10 +0530

    few changes

--- Episode-2-IgnitingOurApp/App.js
+++ Episode-2-IgnitingOurApp/App.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// Important points
 //  why do we need packages ? :- we need lot of superpowers in our React app. React App cannot be a full-fledged production ready app by just using react. It needs lot of superpowers, i.e we need to minify it, we need to bundle it, we need to do chunking, we need to do tree-shaking and we need to do lot of things. so, for that we need packages, those packages will be installed to our project using npm, so we do npm install -D parcel so it installs parcel. Some package we need to install in our global environment and for that we will not use -D.
  
 // Dependency :- Dependency means my project depends on lot of packages  
