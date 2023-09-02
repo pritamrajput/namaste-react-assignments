1b2e0d0dea87eaf5aaafe774c2c445fc2a13cb3e
Pritam Chauhan <rajputpritam619@gmail.com>
2023-09-02 23:03:20 +0530

    Episode 3 code added

--- Episode-3-LayingTheFoundation/App.js
+++ Episode-3-LayingTheFoundation/App.js
@@ -1,14 +1,39 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
-// 1. "Hello world" with HTML
-// 2. "Hello world" with javascript
-// 3. "Hello world" with React
 
-const header1 = React.createElement("h1",{key:"1"},"Namaste everyone");
-const header2 = React.createElement("h2",{key:"2"},"Hello, everyone");
+// How does the JS file executes the JSX code ?
 
-const container = React.createElement("div",{id:"container",key:"3"}, [header1, header2]);
+// This heading2, is a react element.
+const heading2 = (
+    <h1 id="title" key='h2'>
+        Hello, everyone I am namaste react 
+    </h1>
+) 
+
+// React component
+// 1.) Functional component
+// 2.) class based component
+// Functional component
+const HeaderComponent = ()=>{
+   return (
+    <div>
+      <h1>Namaste React functional component</h1>
+      <h1>Hi everyone, Hope you all are doing good</h1>
+    </div>
+   );
+}
+
+const HeaderComponent2 = ()=>(
+    <div>
+       <h1>Namaste React functional component</h1>
+       <h1>Hi everyone, Hope you all are doing good</h1>
+  </div> 
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeaderComponent/>);
+
+
+//1. component composition
+//2. Sanitization
